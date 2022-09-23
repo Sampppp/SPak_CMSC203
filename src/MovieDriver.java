047320230454e6b47e3a948cced6c7f696d5127b
Sampppppppppppppppppppppppp <legosamon@gmail.com>
2022-09-23 01:33:23 -0400

    Task 1 & 2

--- src/MovieDriver.java
+++ src/MovieDriver.java
@@ -2,17 +2,11 @@ import java.util.Scanner;
 
 public class MovieDriver {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		String movieTitle;
-		String movieRating;
-		int ticketNum;
+		Scanner in = new Scanner(System.in);	
 		
 		Movie obj = new Movie();
 		
-		boolean loop = false, error = false;
 		
-		do {
 		System.out.print("Enter the title of the movie: ");
 		obj.setTitle(in.next());
 		System.out.print("Enter the movie's rating: ");
@@ -21,27 +15,7 @@ public class MovieDriver {
 		obj.setSoldTickets(in.nextInt());
 		
 		System.out.println(obj.toString());
-				
-		do {
-			error = false;
-			System.out.print("\nDo you want to enter another movie? (y/n): ");
-
-			switch(in.next().charAt(0)) {
-			case 'Y':
-			case 'y':
-				loop = true;
-				break;
-			case 'N':	
-			case 'n':
-				loop = false;
-				break;
-			default:
-				System.out.println("\nPlease enter a y/n");
-				error = true;
-				break;
-			}
-		} while(error);
-		} while(loop);
+		in.close();
 		System.out.println("Goodbye");
 	}
 }
