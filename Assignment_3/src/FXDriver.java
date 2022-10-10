a436fd2efb80b2c4704ea262c0118c42e38371c8
Sampppppppppppppppppppppppp <legosamson@gmail.com>
2022-10-10 17:57:02 -0400

    finished proj

---
+++ Assignment_3/src/FXDriver.java
@@ -0,0 +1,41 @@
+/*
+ * Class: CMSC203 
+ * Instructor: David Kujit
+ * Description: Caesar and Bellaso encryption and decryption tool
+ * Due: 10/10/2022
+ * Platform/compiler: Eclipse IDE
+ * I pledge that I have completed the programming 
+ * assignment independently. I have not copied the code 
+ * from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here: Samson Pak
+*/
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The main method for the GUI example program JavaFX version
+ * 
+ * @author Farnaz Eivazi
+ * @version 7/11/2022 *
+ */
+public class FXDriver extends Application {
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// call the main scene which is a BorderPane
+		FXMainPane root = new FXMainPane();
+		stage.setScene(new Scene(root, 600, 350));
+		// Set stage title and show the stage.
+		stage.setTitle("Cybersecurity Encryption and Decryption");
+		stage.show();
+
+	}
+}
